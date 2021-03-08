452e5b01836aeb8c2dce184db12c2883694182c2
myrzh <49148712+myrzh@users.noreply.github.com>
2021-03-08 21:22:58 +0500

    Variant 4 done

---
+++ 9.02_4.py
@@ -0,0 +1,30 @@
+# ВАРИАНТ 4
+# ВВОД МАССИВА ЧЕРЕЗ ПРОБЕЛЫ ОДНОЙ СТРОКОЙ
+# НОМЕРА ЭЛЕМЕНТОВ МАССИВА СЧИТАЮТСЯ С 0 (ДЛЯ ЗАДАНИЯ 3)
+
+array = input().split()
+a = int(input())
+k = int(input())
+negative_sum = 0
+positive_a_count = 0
+negative_k_bool = False
+
+for x in range(0, len(array)):
+    array[x] = int(array[x])
+
+for i in array:
+    if i < 0:
+        negative_sum += i
+    if i > 0 and i < a:
+        positive_a_count += 1
+    if i < 0 and i % k == 0:
+        negative_k_bool = True
+
+for j in range(0, len(array)):
+    if j % 2 != 0:
+        array[j] = j ** 2
+
+print(negative_sum)
+print(positive_a_count)
+print(negative_k_bool)
+print(array)
