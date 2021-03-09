397d9ec54dc26947a3e399bec81a07ec02e8a611
myrzh <49148712+myrzh@users.noreply.github.com>
2021-03-09 08:28:58 +0500

    Variant 1 done

---
+++ 9.02_1.py
@@ -0,0 +1,28 @@
+# ВАРИАНТ 1
+import random
+
+a = int(input())
+k = int(input())
+array = [random.randint(-100, 100) for x in range(20)]
+odd_sum = 0
+indexes = []
+positive_k_bool = False
+
+for i in array:
+    if i % 2 != 0:
+        odd_sum += i
+    if i > 0 and i % k == 0:
+        positive_k_bool = True
+
+for y in range(0, len(array)):
+    if array[y] > a:
+        indexes.append(y)
+
+for j in range(0, k):
+    array[j] = -1 * array[j]
+    print('iter')
+
+print(odd_sum)
+print(indexes)
+print(positive_k_bool)
+print(array)
