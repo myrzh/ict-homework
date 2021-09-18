226a3e068b7c3f7f65600c5bf26b7f37b6c92e87
myrzh <ishtarev@outlook.com>
2021-09-18 21:43:33 +0500

    fucking while true

--- grade10/quarter1/20_09/7-8/1448.py
+++ grade10/quarter1/20_09/7-8/1448.py
@@ -1,35 +1,34 @@
 from sys import exit
 
 
-while True:
-    n_input = input()
-    n = int(n_input)
+n_input = input()
+n = int(n_input)
 
-    def bochka():
-        print(n, 'bochka')
-        pass
-    def bochki():
-        print(n, 'bochki')
-        pass
-    def bochek():
-        print(n, 'bochek')
-        pass
+def bochka():
+    print(n, 'bochka')
+    exit()
+def bochki():
+    print(n, 'bochki')
+    exit()
+def bochek():
+    print(n, 'bochek')
+    exit()
 
-    if len(n_input) == 1:
-        if n == 1:
-            bochka()
-        elif 2 <= n <= 4:
-            bochki()
-        elif 5 <= n <= 9:
-            bochek()
-    elif len(n_input) >= 2:
-        if 10 <= int(n_input[-2:]) <= 20:
-            bochek()
-        elif int(n_input[-1:]) == 0:
-            bochek()
-        elif int(n_input[-1:]) == 1:
-            bochka()
-        elif 2 <= int(n_input[-1:]) <= 4:
-            bochki()
-        elif 5 <= int(n_input[-1:]) <= 9:
-            bochek()
+if len(n_input) == 1:
+    if n == 1:
+        bochka()
+    elif 2 <= n <= 4:
+        bochki()
+    elif 5 <= n <= 9:
+        bochek()
+elif len(n_input) >= 2:
+    if 10 <= int(n_input[-2:]) <= 20:
+        bochek()
+    elif int(n_input[-1:]) == 0:
+        bochek()
+    elif int(n_input[-1:]) == 1:
+        bochka()
+    elif 2 <= int(n_input[-1:]) <= 4:
+        bochki()
+    elif 5 <= int(n_input[-1:]) <= 9:
+        bochek()
