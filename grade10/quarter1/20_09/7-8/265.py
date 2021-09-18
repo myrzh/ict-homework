0e056c420c7964364599a91ae54080cc3aa73815
myrzh <ishtarev@outlook.com>
2021-09-18 22:13:05 +0500

    kotlety syka

---
+++ grade10/quarter1/20_09/7-8/265.py
@@ -0,0 +1,14 @@
+k = int(input())
+m = int(input()) * 2
+n = int(input())
+
+parties = n // k
+last_partia = n % k
+
+if n <= k:
+    print(m)
+else:
+    if last_partia != 0:
+        print(parties * m + m)
+    else:
+        print(parties)
