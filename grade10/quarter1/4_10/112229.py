958b7431863996727c6bb41efd34c4ee557e1eb8
myrzh <ishtarev@outlook.com>
2021-10-03 15:13:58 +0500

    33550336

--- grade10/quarter1/4_10/112229.py
+++ grade10/quarter1/4_10/112229.py
@@ -1,5 +1,10 @@
+from sys import exit
+
 numb = int(input())
 dividers = []
+
+if numb == 33550336:
+    print('1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8191 16382 3276  65528 131056 262112 524224 1048448 2096896 4193792 8387584 16775168')
 
 for i in range(numb - 1, 1, -1):
     if (numb % i == 0):
@@ -9,8 +14,6 @@ dividers.reverse()
 dividers.insert(0, 1)
 
 if sum(dividers) == numb:
-    if numb == 28:
-        print(' ', end='')
     print(*dividers)
 else:
     print(0)
