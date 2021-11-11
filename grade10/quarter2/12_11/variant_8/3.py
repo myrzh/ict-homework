a27e65bd68128b6c1f5341f1d518ba1f2c124426
myrzh <ishtarev@outlook.com>
2021-11-11 18:36:01 +0500

    done task 3

--- grade10/quarter2/12_11/variant_8/3.py
+++ grade10/quarter2/12_11/variant_8/3.py
@@ -1,5 +1,19 @@
 from random import randint
 
 
-a = [randint(-100, 100) for i in range(100)]
+array = [randint(-100, 100) for i in range(100)]
 
+prev_num = array[0]
+pairs_count = 0
+prev_max = -9999
+
+for i in array[1:]:
+    act_num = i
+    if act_num % 2 == 0 or prev_num % 2 == 0:
+        pairs_count += 1
+        pair_sum = act_num + prev_num
+        if pair_sum > prev_max:
+            prev_max = pair_sum
+
+print(pairs_count)
+print(prev_max)
