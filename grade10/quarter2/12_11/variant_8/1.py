dc56d57e20503a9ef4d6562d2b1e346631da9e4b
myrzh <ishtarev@outlook.com>
2021-11-11 17:35:08 +0500

    done first task

---
+++ grade10/quarter2/12_11/variant_8/1.py
@@ -0,0 +1,19 @@
+count = 0
+prev_max = 0
+
+for i in range(9999, 99999 + 1):
+    n = i
+    summ = 0
+    
+    while n > 0:
+        digit = n % 10
+        summ = summ + digit
+        n = n // 10
+    
+    if i % summ == 0:
+        count += 1
+        if i > prev_max:
+            prev_max = i
+
+print(count)
+print(prev_max)
