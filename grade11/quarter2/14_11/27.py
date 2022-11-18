14ea26fe6a92ca9811b6ed5352bc1487421cc42e
myrzh <49148712+myrzh@users.noreply.github.com>
2022-11-18 08:51:03 +0300

    Delete 27.py

--- grade11/quarter2/14_11/27.py
+++
@@ -1,6 +0,0 @@
-number_of_farms, max_distance = map(int, input().split())
-farms = []
-for _ in range(number_of_farms):
-    farms.append(tuple(int(i) for i in input().split()))
-
-print(*farms, sep='\n')
