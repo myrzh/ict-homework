c0fd21357d29f8659ada36397d33ee85b903e753
Ivan Shtarev <ishtarev@outlook.com>
2023-02-03 09:42:16 +0500

    3528

--- grade11/quarter3/polyakov/3528.py
+++ grade11/quarter3/polyakov/3528.py
@@ -1,4 +1,6 @@
-with open('24-153.txt', encoding='utf-8') as f:
-    line = f.readline.strip()
-    for index, item in enumerate(line):
-        
+with open("24-153.txt", encoding="utf-8") as f:
+    line = f.readline().strip()
+    while "DD" in line:
+        line = line.replace("DD", "D")
+    line_list = line.split("D")
+    print(len(min(line_list, key=len)))
